ec6b52d52f1fc7c70ef821363d7bd3478d3bcf4e
Seresigo <seresigo@yandex.ru>
2018-05-30 09:17:09 +0300

    add handlebars and localstorage

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ selected_friend_list.addEventListener('click', (e) => {
 /* VK API */
 function vkApi(method, options) {
 	if (!options.v) {
-		options.v = '5.64';
+		options.v = '5.78';
 	}
 
 	return new Promise((resolve, reject) => {
@@ -37,7 +37,7 @@ function vkApi(method, options) {
 function vkInit() { // Инициализация приложения
 	return new Promise((resolve, reject) => {
 		VK.init({
-			apiId: 6334245
+			apiId: 6486204
 		});
 
 		VK.Auth.login(data => { // Логинимся
@@ -49,3 +49,54 @@ function vkInit() { // Инициализация приложения
 		}, 2);
 	});
 }
+
+/* HandelBars */
+let template = `
+{{#each items}}
+    <li draggable="true" id="{{id}}" ondragstart="return dragStart(event)" class="item">
+        <img src="{{photo_200}}" alt="img">
+        <p>{{first_name}} {{last_name}}</p>
+        <div class="plus">✚</div>
+    </li>
+{{/each}}
+`;
+let templateFn = Handlebars.compile(template);
+
+new Promise(resolve => window.onload = resolve)
+    .then(() => vkInit())
+    .then(() => vkApi('friends.get', {fields: 'photo_200'}))
+    .then(response => {
+        for(let i = 0; i < response.items.length; i++){
+            if(!response.items[i].photo_200){
+                response.items[i].photo_200 = 'img/1.png';
+            }
+        } 		// При первой загрузке страницы загружает первый список
+        document.querySelector('.common_friends_list ul').innerHTML = templateFn(response);
+        return new Promise((resolve) => {
+
+            /* Вывести из хранилища если есть там что то*/
+            if(localStorage.data){
+                let arrSelected = JSON.parse(localStorage.data);
+                resolve(arrSelected);
+            }
+        });
+    })
+    .then( (arrSelected) => {
+        for (let prop in arrSelected) {
+            document.getElementById(arrSelected[prop]).lastElementChild.setAttribute('class', 'remove');
+            document.getElementById('selected_friend_list').appendChild(document.getElementById(arrSelected[prop]));
+        }
+    })
+    .catch(e => alert('Ошибка: ' + e.message));
+
+/* localStorage */
+let storage = localStorage;
+let reestablish = {};
+
+document.querySelector('.save a').addEventListener('click', () => {
+    let arrLi = document.querySelectorAll('.selected_friend_list ul li');
+    for(let i = 0; i < arrLi.length; i++){
+        reestablish[i] = arrLi[i].getAttribute('id');
+    }
+    storage.data = JSON.stringify(reestablish);
+});
